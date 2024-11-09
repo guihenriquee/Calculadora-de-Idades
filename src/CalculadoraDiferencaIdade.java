ce1f1477e706175a301c071c9116cbb7fd44a088
Guilherme Henrique <guilhermehenriquemraissilva@gmail.com>
2024-11-09 11:18:34 -0300

    Add files via upload

---
+++ src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,15 @@
+/*Crie uma classe chamada CalculadoraDiferencaIdade
+ * Crie uma variável para guardar a primeira idade
+ * Crie uma variável para guardar a segunda idade
+ * Crie uma variável para guardar a diferença entre a primeira e a segunda idade
+ * Imprima a diferença no console
+ */
+
+public class CalculadoraDiferencaIdade {
+    public static void main(String[] args){
+        int primeiraIdade = 30;
+        int segundaIdade = 20;
+        int diferencaIdades = primeiraIdade - segundaIdade;
+        System.out.println("A diferenca entre idades eh: " + diferencaIdades);
+    }
+}
